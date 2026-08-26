7957ec0d0e5809c5ca95e8fbae88967e84e511ec
misbahulamin <misbahul.amin2001@gmail.com>
2026-08-26 18:39:03 +0800

    added the rds

--- core/settings/base.py
+++ core/settings/base.py
@@ -5,7 +5,7 @@ from decouple import Csv, config
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-befood-development-key')
 DEBUG = False
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["api.befood.com.bd",'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
--- core/settings/prod.py
+++ core/settings/prod.py
@@ -1,7 +1,21 @@
 from .base import *
 DEBUG = False
 ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', default='', cast=Csv())
-DATABASES = {'default': {'ENGINE': 'django.db.backends.postgresql', 'NAME': config('DB_NAME'), 'USER': config('DB_USER'), 'PASSWORD': config('DB_PASSWORD'), 'HOST': config('DB_HOST', default='localhost'), 'PORT': config('DB_PORT', default='5432')}}
+
+
+# DATABASES = {'default': {'ENGINE': 'django.db.backends.postgresql', 'NAME': config('DB_NAME'), 'USER': config('DB_USER'), 'PASSWORD': config('DB_PASSWORD'), 'HOST': config('DB_HOST', default='localhost'), 'PORT': config('DB_PORT', default='5432')}}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'befood_db',       # Database name you created on RDS
+        'USER': 'befood_postgres',   # The username you set for RDS
+        'PASSWORD': 'Befood459',  # The password you set for RDS
+        'HOST': 'befood-postgres-prod.c56oegiikk4d.ap-south-1.rds.amazonaws.com',  # RDS endpoint
+        'PORT': '5432',  # Default PostgreSQL port
+    }
+}
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
